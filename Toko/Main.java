e4a79b9c53df17154a02b6c749cfdb7a339b54e2
rifqiabrar <mrifqiabrarr@gmail.com>
2026-05-05 16:49:00 +0700

    first commit

---
+++ Toko/Main.java
@@ -0,0 +1,16 @@
+package Toko;
+import base.Produk;
+
+
+public class Main {
+    public static void main (String args []){
+        Produk p = new Produk("Indomie Goreng", 3500, 100, "Makanan");
+
+        System.out.println(p.stok);
+        System.out.println(p.harga);
+        System.out.println(p.kategori);
+        System.out.println(p.nama);
+
+        p.tampilInfo();
+    }
+}
