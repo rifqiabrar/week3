c845229d097933ab4724b0de3ab3e13b5be34d01
rifqiabrar <mrifqiabrarr@gmail.com>
2026-05-11 22:11:21 +0700

    menambahkan transaksi

--- Main.java
+++ Main.java
@@ -46,9 +46,11 @@ public class Main {
         System.out.println("  Diskon   : Rp" + roti.hitungDiskon(5, 15));
         System.out.println("  Total    : Rp" + (roti.hitungTotalHarga(5) - roti.hitungDiskon(5, 15)));
 
-        
-
-
-        
+        // --- Transaksi --------------------------------------------------------
+        System.out.println("\n====== TRANSAKSI ======");
+        laptop.beli(1);
+        hp.beli(2);
+        indomie.beli(10);
+        roti.beli(5);    
     }
 }
